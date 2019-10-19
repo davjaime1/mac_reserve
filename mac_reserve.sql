eca26b46de573f299e1cfef4bd2da4f0f3ebfabd
Jaime, David <davjaime1@yahoo.com>
2019-10-19 02:41:05 -0500

    Update mac_reserve.sql
    Added Tables to database
    - Facilites Table
    - Facilityreservation
    - facilitytypes
    - intervals
    - venues

--- mac_reserve.sql
+++ mac_reserve.sql
@@ -16,6 +16,127 @@
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
+-- Table structure for table `facilities`
+--
+
+DROP TABLE IF EXISTS `facilities`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `facilities` (
+  `name` varchar(45) NOT NULL,
+  `facilitytype` varchar(45) NOT NULL,
+  `interval` varchar(45) NOT NULL,
+  `duration` varchar(45) NOT NULL,
+  `venue` varchar(45) NOT NULL,
+  `deposit` varchar(3) NOT NULL,
+  PRIMARY KEY (`name`),
+  KEY `fk_facilitytype_idx` (`facilitytype`),
+  KEY `fk_interval` (`interval`),
+  KEY `fk_duration_idx` (`duration`),
+  KEY `fk_venue_idx` (`venue`),
+  CONSTRAINT `fk_duration` FOREIGN KEY (`duration`) REFERENCES `durations` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  CONSTRAINT `fk_facilitytype` FOREIGN KEY (`facilitytype`) REFERENCES `facilitytypes` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  CONSTRAINT `fk_interval` FOREIGN KEY (`interval`) REFERENCES `intervals` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  CONSTRAINT `fk_venue` FOREIGN KEY (`venue`) REFERENCES `venues` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `facilities`
+--
+
+LOCK TABLES `facilities` WRITE;
+/*!40000 ALTER TABLE `facilities` DISABLE KEYS */;
+INSERT INTO `facilities` VALUES ('BMC1','BMC','30M','SD','I','$25'),('BMC10','BMC','30M','SD','I','$25'),('BMC2','BMC','30M','SD','I','$25'),('BMC3','BMC','30M','SD','I','$25'),('BMC4','BMC','30M','SD','I','$25'),('BMC5','BMC','30M','SD','I','$25'),('BMC6','BMC','30M','SD','I','$25'),('BMC7','BMC','30M','SD','I','$25'),('BMC8','BMC','30M','SD','I','$25'),('BMC9','BMC','30M','SD','I','$25'),('CR1','CR','1H','SD','I','$50'),('CR2','CR','1H','SD','I','$50'),('CR3','CR','1H','SD','I','$50'),('CR4','CR','1H','SD','I','$50'),('CR5','CR','1H','SD','I','$50'),('IBBC1','IBBC','1H','SD','I','$25'),('IBBC2','IBBC','1H','SD','I','$25'),('IBBC3','IBBC','1H','SD','I','$25'),('IBBC4','IBBC','1H','SD','I','$25'),('IBBC5','IBBC','1H','SD','I','$25'),('IVBC1','IVBC','1H','SD','I','$25'),('IVBC2','IVBC','1H','SD','I','$25'),('IVBC3','IVBC','1H','SD','I','$25'),('IVBC4','IVBC','1H','SD','I','$25'),('IVBC5','IVBC','1H','SD','I','$25'),('IVBC6','IVBC','1H','SD','I','$25'),('IVBC7','IVBC','1H','SD','I','$25'),('IVBC8','IVBC','1H','SD','I','$25'),('IVBC9','IVBC','1H','SD','I','$25'),('MR1','MR','1H','SD','I','$50'),('MR2','MR','1H','SD','I','$50'),('MR3','MR','1H','SD','I','$50'),('MR4','MR','1H','SD','I','$50'),('OBBC1','OBBC','2H','7D','O','$100'),('OBBC2','OBBC','2H','7D','O','$100'),('OVBC1','OVBC','2H','7D','O','$100'),('OVBC2','OVBC','2H','7D','O','$100'),('RBC1','RBC','30M','SD','I','$25'),('RBC2','RBC','30M','SD','I','$25'),('RBC3','RBC','30M','SD','I','$25'),('RBC4','RBC','30M','SD','I','$25'),('RBC5','RBC','30M','SD','I','$25'),('SCG','SCG','2H','SD','I','$50'),('TT1','TT','30M','SD','I','$50');
+/*!40000 ALTER TABLE `facilities` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `facilityreservation`
+--
+
+DROP TABLE IF EXISTS `facilityreservation`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `facilityreservation` (
+  `reservationid` varchar(16) NOT NULL,
+  `facilityname` varchar(45) NOT NULL,
+  `facilitytype` varchar(45) NOT NULL,
+  `venue` varchar(45) NOT NULL,
+  `reservedUser` varchar(45) NOT NULL,
+  `date` date NOT NULL,
+  `to` datetime NOT NULL,
+  `from` datetime NOT NULL,
+  PRIMARY KEY (`reservationid`),
+  KEY `fk_fr_facilityname_idx` (`facilityname`),
+  KEY `fk_fr_facilitytype_idx` (`facilitytype`),
+  KEY `fk_fr_venue_idx` (`venue`),
+  KEY `fk_fr_reservedUser_idx` (`reservedUser`),
+  CONSTRAINT `fk_fr_facilityname` FOREIGN KEY (`facilityname`) REFERENCES `facilities` (`name`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  CONSTRAINT `fk_fr_facilitytype` FOREIGN KEY (`facilitytype`) REFERENCES `facilitytypes` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  CONSTRAINT `fk_fr_reservedUser` FOREIGN KEY (`reservedUser`) REFERENCES `users` (`username`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  CONSTRAINT `fk_fr_venue` FOREIGN KEY (`venue`) REFERENCES `venues` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `facilityreservation`
+--
+
+LOCK TABLES `facilityreservation` WRITE;
+/*!40000 ALTER TABLE `facilityreservation` DISABLE KEYS */;
+/*!40000 ALTER TABLE `facilityreservation` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `facilitytypes`
+--
+
+DROP TABLE IF EXISTS `facilitytypes`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `facilitytypes` (
+  `id` varchar(45) NOT NULL,
+  `name` varchar(45) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `facilitytypes`
+--
+
+LOCK TABLES `facilitytypes` WRITE;
+/*!40000 ALTER TABLE `facilitytypes` DISABLE KEYS */;
+INSERT INTO `facilitytypes` VALUES ('BMC','Badminton Court'),('CR','Conference Room'),('IBBC','Indoor Basketball Court'),('IVBC','Volleyball Court'),('MR','Multipurpose Room'),('OBBC','Outdoor Basketball Court'),('OVBC','Outdoor Volleyball Court'),('RBC','Racquetball Court'),('SCG','Indoor Soccer Gymnasium'),('TT','Table Tennis');
+/*!40000 ALTER TABLE `facilitytypes` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `intervals`
+--
+
+DROP TABLE IF EXISTS `intervals`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `intervals` (
+  `id` varchar(45) NOT NULL,
+  `name` varchar(45) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `intervals`
+--
+
+LOCK TABLES `intervals` WRITE;
+/*!40000 ALTER TABLE `intervals` DISABLE KEYS */;
+INSERT INTO `intervals` VALUES ('1H','1 hour'),('2H','2 hours'),('30M','30 min');
+/*!40000 ALTER TABLE `intervals` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
 -- Table structure for table `roles`
 --
 
@@ -99,6 +220,31 @@ LOCK TABLES `users` WRITE;
 /*!40000 ALTER TABLE `users` DISABLE KEYS */;
 /*!40000 ALTER TABLE `users` ENABLE KEYS */;
 UNLOCK TABLES;
+
+--
+-- Table structure for table `venues`
+--
+
+DROP TABLE IF EXISTS `venues`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `venues` (
+  `id` varchar(45) NOT NULL,
+  `name` varchar(45) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `venues`
+--
+
+LOCK TABLES `venues` WRITE;
+/*!40000 ALTER TABLE `venues` DISABLE KEYS */;
+INSERT INTO `venues` VALUES ('I','Indoor'),('O','Outdoor');
+/*!40000 ALTER TABLE `venues` ENABLE KEYS */;
+UNLOCK TABLES;
+
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
